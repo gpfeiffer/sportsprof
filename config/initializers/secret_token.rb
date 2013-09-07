f3563169491cd12836c555b89cce448697a16b46
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2013-09-07 13:37:28 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportsprof::Application.config.secret_token = '92b41a6a751d533ca456399d428d73bcfac6e2c50fd54d13b4cba5274a23a748f977d6be0262f033ca764a83059376554591405372c9afa2540490ad39dd10d0'
